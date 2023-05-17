604665c59305aa0f60940c1dc5cf7d929c570019
Andrjuha-joma-jozhik <andrey.zhmudenko1@gmail.com>
2023-05-17 15:46:38 +0300

    add: storybook, styled-component

---
+++ .storybook/main.ts
@@ -0,0 +1,23 @@
+import type { StorybookConfig } from "@storybook/react-vite";
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+  core: {
+    builder: '@storybook/builder-vite', // 👈 The builder enabled here.
+  },
+  typescript: {
+    reactDocgen: 'react-docgen', // 👈 react-docgen configured here.
+  },
+};
+export default config;
